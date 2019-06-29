2be32a2859a1263576481917c7e3d8b408113b40
CapLiu <584844970@qq.com>
2019-06-29 17:19:03 +0800

    use trace to log

--- apps/user_app/login.py
+++ apps/user_app/login.py
@@ -3,17 +3,21 @@ import tornado.ioloop
 import tornado.web
 import os
 from apps.appsettings import generalsettings
+from util.log.trace import Trace
 class userLoginHandler(tornado.web.RequestHandler):
+    __tracer = Trace('user_app')
     def get_template_path(self):
         return os.path.join(generalsettings['templates'], 'user')
 
     def get(self):
         #content_dict = {'username': ''}
+        self.__tracer.trace('user app get method',streamlevel=20,filelevel=20)
         self.render('login.html',myusername='')
 
     def post(self):
         username = self.get_argument('username')
         password = self.get_argument('password')
+        self.__tracer.trace('user app post method', streamlevel=20, filelevel=20)
         content = {'myusername':username}
         self.render('login.html',**content)
 
